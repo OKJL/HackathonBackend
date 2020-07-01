d27443de1f616c40897cb7c40fde6281f9ed92bb
Aaron Teo <aaron.teo@riv-alumni.com>
2020-07-01 12:31:40 +0800

    Fixed Jest testing

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ["<rootDir>/src/tests"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  reporters: [
+    "default",
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        includeFailureMsg: true,
+        statusIgnoreFilter: "pending",
+        dateFormat: "dd-mm-yyyy HH:MM:ss",
+        outputPath: "ui-report/index.html",
+        pageTitle: "Firestore Rules Unit Test Result",
+      },
+    ],
+  ],
+  testEnvironment: "node",
+}
